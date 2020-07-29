90c55c4ebc2880abe58367edd7fca4dc154622cc
Route9Library <you@example.com>
2020-07-29 16:32:43 -0400

    simple

---
+++ calcs.js
@@ -0,0 +1,40 @@
+// console.log("Code Differently");
+
+// console.log(12+30);
+
+// console.log(3-5);
+
+// console.log(3*3);
+
+// console.log(12/2);
+
+// var x = 20;
+// var y = 5;
+
+// console.log(x+y);
+
+// console.log(x-y);
+
+// console.log(x*y);
+
+// console.log(x/y);
+
+// for(var i=0; i<5; i++){
+//     console.log(i);
+// }
+
+var x = 72;
+var y = 8;
+
+console.log(x+y);
+
+console.log(x-y);
+
+console.log(x*y);
+
+console.log(x/y);
+
+for(var i=5; i<=15; i+=2){
+    console.log(i);
+}
+
